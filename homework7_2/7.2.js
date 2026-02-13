1fdfa5196b06d1b9917be2800165f24883ce79dd
1937Alibek <zh.alibek2010@gmail.com>
2026-02-13 18:38:07 +0500

    homework

---
+++ homework7_2/7.2.js
@@ -0,0 +1,29 @@
+let eeee=5;
+console.log(eeee);
+eeee="денег"
+console.log(eeee)
+const cons=true;
+console.log(cons);
+// cons=false;
+// console.log(cons);//
+var varity = 0/0;
+console.log(varity);
+
+varity = null;
+console.log(varity);
+let еее_pyat=Number(eeee);
+console.log(eeee);
+let deneg=5;
+let str_deneg=String(deneg);
+console.log(str_deneg);
+let haz = null;
+console.log(String(haz));
+let bik=undefined;
+console.log(Number(bik));
+console.log(String(str_deneg))
+let a = "1937"
+let str = Number(a)
+console.log(str)
+prompt("Напишите что то")
+// var лучше не использовть из за не надежного и не предсказуемого поведения 
+// const Не может быть изменена или повторно объявлена.

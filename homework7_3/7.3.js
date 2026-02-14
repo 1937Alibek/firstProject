e1f85cf889a2efd1229b2e6b5ea6046e47b51760
1937Alibek <zh.alibek2010@gmail.com>
2026-02-14 18:51:29 +0500

    homework

---
+++ homework7_3/7.3.js
@@ -0,0 +1,26 @@
+let a = 10;
+let b = 5;
+let c = 2;
+console.log(a + b + c);
+console.log(a + c - b);
+console.log(a * b / c);
+console.log(a % b);
+let x = 7;
+console.log(++x)
+console.log(x++)
+console.log(--x)
+console.log(x--)
+let str = "The result is: "
+let d = 4
+let f = 5
+console.log(str + (d + f))
+console.log(typeof (str + (d + f)))
+console.log(a > b && a > 0 && b > 0)
+console.log(c < 10 || c == 2);
+console.log(a / c != 5)
+console.log(10 == "10")
+console.log(10 === "10")
+// === это строгое равенство оно считывает тип данных из за чего выводит в консоль false
+console.log(x % 2 ==0 || x % 3 == 0 && x % 6 != 0)
+let y = 1937
+console.log(y > 10 || y < 5)

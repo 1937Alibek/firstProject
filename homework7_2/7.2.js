006e6d2d16e97e8a33c84633cde5c4da55d7ede7
1937Alibek <zh.alibek2010@gmail.com>
2026-02-14 18:19:58 +0500

    homework

--- homework7_2/7.2.js
+++ homework7_2/7.2.js
@@ -8,8 +8,11 @@ console.log(cons);
 // console.log(cons);//
 var varity = 0/0;
 console.log(varity);
-
+console.log(typeof(varity))
+console.log(typeof(eeee))
+console.log(typeof(cons))
 varity = null;
+console.log(typeof(varity))
 console.log(varity);
 let еее_pyat=Number(eeee);
 console.log(eeee);
@@ -24,6 +27,9 @@ console.log(String(str_deneg))
 let a = "1937"
 let str = Number(a)
 console.log(str)
-prompt("Напишите что то")
+let name=prompt("введите имя")
+let age=(prompt("введите возраст"))
+console.log(name)
+console.log(age)
 // var лучше не использовть из за не надежного и не предсказуемого поведения 
 // const Не может быть изменена или повторно объявлена.
